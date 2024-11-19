69f01d03fa74fb2edb135a89db02930d86950e61
felipecifonseca1 <felipe.cifonseca@usp.br>
2024-11-19 08:52:43 -0300

    apenas com views funcionais, sem utilizar Django forms e sem validação de dados

---
+++ meu_blog/blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-date_posted')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')  
+
+        post = Post.objects.create(title=title, content=content, image=image)  
+        return redirect('post-detail', pk=post.id)
+    return render(request, 'blog/post_form.html')
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        if 'image' in request.FILES:
+            post.image = request.FILES['image']  
+        post.save() 
+        return redirect('post-detail', pk=post.id)
+    return render(request, 'blog/post_form.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post-list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
