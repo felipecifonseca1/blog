25913664afd1aef24cf0fa2caf9fabc2a049c41f
felipecifonseca1 <felipe.cifonseca@usp.br>
2024-11-19 20:40:30 -0300

    Sistema de comentários implementado com usuario logado

--- meu_blog/blog/views.py
+++ meu_blog/blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
@@ -13,28 +14,34 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Adicionar os comentários do post ao contexto
+        context['comments'] = self.object.comments.all().order_by('-date_posted')
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()  # Obtem o post atual
+        content = request.POST.get('content')
+        if content:
+            Comment.objects.create(
+                post=self.object,
+                author=request.user,
+                content=content
+            )
+        return redirect('post-detail', pk=self.object.pk)
+
 class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content', 'image']  
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('post-list')
 
-    # def get_form(self, *args, **kwargs):
-    
-    #     form = super().get_form(*args, **kwargs)
-    #     form.enctype = 'multipart/form-data'
-    #     return form
-
 class PostUpdateView(UpdateView):
     model = Post
     fields = ['title', 'content', 'image']  
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('post-list')
-
-    # def get_form(self, *args, **kwargs):
-    #     form = super().get_form(*args, **kwargs)
-    #     form.enctype = 'multipart/form-data'
-    #     return form
 
 class PostDeleteView(DeleteView):
     model = Post
